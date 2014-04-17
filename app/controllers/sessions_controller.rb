c7630641f436c50a662338547154027b6c93cfe7
Blair Anderson <Blair@quickleft.com>
2014-04-16 21:45:13 -0600

    basic login logout functionality

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,33 @@
+class SessionsController < ApplicationController
+  after_filter :logout_user, only: [:destroy]
+
+  def create
+    @user = User.where(email: user_params[:email]).first
+    login_user
+  end
+
+
+  def destroy
+    @user = User.find(session[:user_id])
+    if @user
+      render json: {message: "Logged Out!"}, status: 204
+    else
+      render json: {error: "User Not Found" }, status: 404
+    end
+  end
+
+private
+  def login_user
+    if @user
+      @current_user = @user
+      session[:user_id] = @user.id
+      redirect_to inventory_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  def logout_user
+    session[:user_id] = nil
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,10 +19,4 @@ class UsersController < ApplicationController
   	  render :json => { :errors => @user.errors.full_messages }, :status => 422
   	end
   end
-
-  private
-
-	def user_params
-	  params.require(:user).permit(:email)
-	end
 end
